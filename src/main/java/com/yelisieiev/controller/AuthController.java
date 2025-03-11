919187f7d0a9f0db6a60ef3fc77524bff5889fda
Alex <taus.anarchy@gmail.com>
2025-03-11 23:46:18 +0200

    Add auth

---
+++ src/main/java/com/yelisieiev/controller/AuthController.java
@@ -0,0 +1,38 @@
+package com.yelisieiev.controller;
+
+import com.yelisieiev.model.User;
+import com.yelisieiev.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+public class AuthController {
+
+    private final UserService userService;
+
+    @Autowired
+    public AuthController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping("/register")
+    public String showRegistrationForm(Model model) {
+        model.addAttribute("user", new User());
+        return "auth/register";
+    }
+
+    @PostMapping("/register")
+    public String registerUser(@ModelAttribute("user") User user) {
+        userService.registerNewUser(user);
+        return "redirect:/login?registered";
+    }
+
+    @GetMapping("/login")
+    public String showLoginForm() {
+        return "auth/login";
+    }
+}
